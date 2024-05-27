48ed2e9937b81a8abae73e3ca1cb7410ced53c07
AmanSangani <sanganiaman1310@gmail.com>
2024-05-27 11:22:06 +0530

    vite config updated for material Tailwind UI

--- reactPart1/vite.config.js
+++ reactPart1/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ["@material-tailwind/react"]
+  },
+  build: {
+    rollupOptions: {
+      external: ['@material-tailwind/react']
+    }
+  }
 })
